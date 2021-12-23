07223fa6af85b0db4c6236ef6de27ca424386af8
wizard <10930292@dcsh.tp.edu.tw>
2021-12-23 15:16:52 +0800

    刪除討論版

--- forum/settings.py
+++ forum/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'forum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['pagetmpl'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/' 
+LOGIN_URL= '/user/login/'
